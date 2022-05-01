b6cfb7b1453542f4ede049ead7377b593744cbdf
Lucas4d4 <101574260+Lucas4d4@users.noreply.github.com>
2022-05-01 11:56:30 -0300

    Nuevo diseño de la página

--- TP5_GRUPO_2/EliminarSucursal.aspx
+++ TP5_GRUPO_2/EliminarSucursal.aspx
@@ -6,11 +6,59 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 291px;
+        }
+        .auto-style3 {
+            width: 128px;
+        }
+        .auto-style4 {
+            width: 397px;
+        }
+        .auto-style5 {
+            width: 259px;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">
+                        <asp:HyperLink ID="hlAgregarSucursal" runat="server" NavigateUrl="~/AgregarSucursal.aspx">Agregar sucursal</asp:HyperLink>
+                    </td>
+                    <td class="auto-style5">
+                        <asp:HyperLink ID="hlListarSucursal" runat="server" NavigateUrl="~/ListarSucursal.aspx">Listado de sucursales</asp:HyperLink>
+                    </td>
+                    <td>
+                        <asp:HyperLink ID="hlEliminarSucursal" runat="server" NavigateUrl="~/EliminarSucursal.aspx">Eliminar sucursal</asp:HyperLink>
+                    </td>
+                </tr>
+            </table>
         </div>
+        <p>
+            <asp:Label ID="lblEliminarS" runat="server" Font-Bold="True" Font-Size="18pt" Text="Eliminar sucursales"></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style3">
+                    <asp:Label ID="lblIDEliminar" runat="server" Text="Ingrese ID sucursal:"></asp:Label>
+                </td>
+                <td class="auto-style4">
+                    <asp:TextBox ID="txtEliminarID" runat="server" Width="378px"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:Button ID="btnEliminarID" runat="server" Text="Eliminar" />
+                </td>
+            </tr>
+        </table>
     </form>
 </body>
 </html>
--- TP5_GRUPO_2/EliminarSucursal.aspx.designer.cs
+++ TP5_GRUPO_2/EliminarSucursal.aspx.designer.cs
@@ -22,5 +22,68 @@ namespace TP5_GRUPO_2
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control hlAgregarSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hlAgregarSucursal;
+
+        /// <summary>
+        /// Control hlListarSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hlListarSucursal;
+
+        /// <summary>
+        /// Control hlEliminarSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hlEliminarSucursal;
+
+        /// <summary>
+        /// Control lblEliminarS.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEliminarS;
+
+        /// <summary>
+        /// Control lblIDEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblIDEliminar;
+
+        /// <summary>
+        /// Control txtEliminarID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEliminarID;
+
+        /// <summary>
+        /// Control btnEliminarID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminarID;
     }
 }
